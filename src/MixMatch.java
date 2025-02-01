4b4f1207aa75bab6de4a623facee95801d00e864
Ricardo P. <perez.ricardo.p1@gmail.com>
2025-02-01 06:43:55 +0000

    Done

---
+++ src/MixMatch.java
@@ -0,0 +1,17 @@
+public class MixMatch{
+
+    public static void main(String[] args){
+    int mynumber = 7;        //declaring the 8 different types of variables 
+    String word = "Hello"; 
+    float decimalLong = 7.3333f; 
+    double decimal = 7.33; 
+    char character = 'T'; 
+    boolean truthVar = true; 
+    short myNum = 22; 
+    long randNum = 222;
+
+        System.out.println(mynumber + ": an Integer that can store numbers from  -2,147,483,648 to 2,147,483,647. " + word + ": a string variable that can contain any ammount of characters and spaces. Used to store text. " + decimalLong + ": a decimal (float) variable that stores decimals up to 7 decimal places. Must end with f whenever creating floats. " + decimal + ": a double type variable with double the decimal precision as a float type, 15 decimal places. " + character + ": a character variable type that stores a single character. " + truthVar + ": boolean variables hold truth values. True or false. " + myNum + ": the data type short holds small integers from -32,768 to 32,767. " + randNum + ": the long data type stores whole numbers from -2^63 to 2^63 -1 "
+     );
+    }
+}
+
